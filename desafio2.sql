e67c31611efe94ba17567a799573acd6fdb3875f
Matheus de Araújo Alves <matheus.aalves@MacBook-Air-de-Matheus.local>
2022-04-12 11:15:49 -0300

    Requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+SELECT
+    COUNT(DISTINCT mu.musica_id) AS cancoes,
+    COUNT(DISTINCT ar.artista_id) AS artistas,
+    COUNT(DISTINCT al.album_id) AS albuns
+FROM SpotifyClone.album AS al
+INNER JOIN SpotifyClone.artista AS ar ON al.artista_id = ar.artista_id
+INNER JOIN SpotifyClone.musica AS mu ON mu.album_id = al.album_id
